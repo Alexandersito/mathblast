2a43fe8621bd8273c6900992992587f3b220d487
Alexandersito <018101396i@uandina.edu.pe>
2024-08-22 00:44:39 -0500

    ajuste ruta

--- controladores/ruta.controlador.php
+++ controladores/ruta.controlador.php
@@ -4,13 +4,13 @@ class ControladorRuta
 {
     public static function ctrRuta()
     {
-        return "https://mathblast.onrender.com/";
+        return "https://mathblast-production.up.railway.app/";
         // return "http://localhost/mathblast/";
     }
 
     public static function ctrServidor()
     {
-        return "https://mathblast.onrender.com/backend/";
+        return "https://mathblast-production.up.railway.app/backend/";
         // return "http://localhost/mathblast/backend/";
     }
 }
