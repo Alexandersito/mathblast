23b2a238937099186cec39539f4ff860fc772225
Alexandersito <018101396i@uandina.edu.pe>
2024-08-22 00:53:55 -0500

    ajustes db

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -16,13 +16,32 @@
 
 // }
 
-Class Conexion
+// Class Conexion
+// {
+//     public static function conectar()
+//     {
+//         $link = new PDO("mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_DATABASE'),
+//             getenv('DB_USERNAME'),
+//             getenv('DB_PASSWORD'));
+
+//         $link->exec("set names utf8");
+
+//         return $link;
+//     }
+// }
+
+class Conexion
 {
     public static function conectar()
     {
-        $link = new PDO("mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_DATABASE'),
-            getenv('DB_USERNAME'),
-            getenv('DB_PASSWORD'));
+        $host = getenv('DB_HOST');
+        $port = getenv('DB_PORT');
+        $dbname = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $password = getenv('DB_PASSWORD');
+
+        $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+        $link = new PDO($dsn, $user, $password);
 
         $link->exec("set names utf8");
 
