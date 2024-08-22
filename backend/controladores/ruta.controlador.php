54c8e15fc49dbbb44c029d7d5eba46a6e76403cb
Alexandersito <018101396i@uandina.edu.pe>
2024-08-22 01:00:48 -0500

    rutas

--- backend/controladores/ruta.controlador.php
+++ backend/controladores/ruta.controlador.php
@@ -5,14 +5,14 @@ class ControladorRuta{
 	static public function ctrRuta(){
 
 		// return "http://localhost/mathblast/";
-		return "https://mathblast.onrender.com/";
+		return "https://mathblast-production.up.railway.app/";
 
 	}
 
 	static public function ctrRutaBackend(){
 
 		// return "http://localhost/mathblast/backend/";
-		return "https://mathblast.onrender.com/backend/";
+		return "https://mathblast-production.up.railway.app/backend/";
 
 	}
 
